314e1e5f802ca96e5f0d59f41c05aa10cfb9a00b
francky59200 <49784881+francky59200@users.noreply.github.com>
2019-08-13 16:48:38 +0200

    Travaux sites HTML CSS JS

---
+++ HTML5_CSS3/exoifocop7/form.php
@@ -0,0 +1,15 @@
+<?php
+   print '<pre>';
+   print_r ($_POST);
+   print '</pre>';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Données du formulaire</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+    <p><a href="index.html">Retour au formulaire</a></p>
+   </body>
+</html>
