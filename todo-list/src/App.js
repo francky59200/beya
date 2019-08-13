4cd22ca1957c2d93165123f3c5d7153ddaee82df
francky59200 <befranck74@gmail.com>
2019-08-14 01:49:09 +0200

    compteur dynamique

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  state ={
+    client: [
+      "jean",
+      "noemie",
+      "patrick"
+    ],
+    compteur: 0,
+    nouveauClient:""
+  }
+handleCompt = () =>{
+ const chrono =this.state.compteur +1;
+ this.setState({compteur:chrono});
+}
+
+handleDelete =() =>{
+  const suppr = this.state.client.slice();
+  const index = suppr.findIndex((client)=> client);
+  suppr.splice(index, 1);
+  this.setState({client:suppr});
+}
+
+  render () {
+    const clients= this.state.client.map((client) =><li>{client}<button onClick={() =>this.handleDelete(client)}>x</button></li>);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <li>{this.state.client}</li>
+      {clients}
+      {this.state.compteur}<button onClick={this.handleCompt}>compteur</button>
+      <form>
+        <input type="text" placeholder="nouveau client"></input>
+          <button>Ajouter</button>
+      </form>
+      
     </div>
   );
+  }
 }
 
 export default App;
