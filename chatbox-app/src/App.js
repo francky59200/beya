c932a7e80b1622104058f1355cada3e7ce62a0a3
francky59200 <befranck74@gmail.com>
2019-09-07 01:39:46 +0200

    CSSTransition (React)

--- chatbox-app/src/App.js
+++ chatbox-app/src/App.js
@@ -2,10 +2,15 @@ import React, { Component, createRef } from 'react'
 import Formulaire from './components/Formulaire'
 import Message from './components/Message'
 import './App.css'
+import './Animations.css'
 
 //Firebase
 
 import base from './base'
+
+// Animations
+
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 
 class App extends Component {
   
@@ -13,8 +18,13 @@ class App extends Component {
     messages:{},
     pseudo: this.props.match.params.pseudo // cette valeur a été trouvé grace a addon ract  dans la console
   }
+   
+    // on cree une ref qui servira a recuperer le current pour le scroll
 
   messageRef = createRef() // on cree la ref car on veut eviter le scroll lors d'un nouveau message
+
+
+  // on monte l'application pour synchroniser notre firebase avec l'élément a sauvegarder qui est  messages
 
   componentDidMount () {
     base.syncState('/', { // ici on synchronise notre state avec firebase a la racine '/'
@@ -23,10 +33,14 @@ class App extends Component {
     })
   }
 
+// c'est lors de la mise a jour du state messages qu'on modifie le scroll des message
+
   componentDidUpdate () { // le scroll s'utulise lors du cycle mise a jour de l'application
     const ref= this.messageRef.current // on recupére le current des messages
     ref.scrollTop = ref.scrollHeight // et on applique une comparaison pour que le scroll reste toujours en bas
   }
+
+  //ajout des messages avec une clé timestamp pour les identifier
   
   addMessage= message =>{ // on creer une methode pour l'ajout des messages
     const messages= { ...this.state.messages} // une copie du tableau
@@ -41,22 +55,32 @@ class App extends Component {
       })
     this.setState({messages})// et on modifie le state
   }
+  
+  // on creer une fonction pour differencier les differents user
+  isUser = pseudo => pseudo === this.state.pseudo
 
   render () {
   // on creer une constante avec la clé objet de notre state on lui fait une boucle de cette clé
   //en reprenant la clé du message et du pseudo mise a jour dans le state
     const messages = Object.keys(this.state.messages) 
-                     .map(key => (<Message
-                      Key={key}
+                     .map(key => (
+                      <CSSTransition
+                       timeout={2000}
+                       classNames='anim'
+                       Key={key}>
+                      <Message
+                      isUser={this.isUser} //ici on applique la props qui a été envoyé a message.js
                       message={this.state.messages[key].message}
-                      pseudo={this.state.messages[key].pseudo} />))
+                      pseudo={this.state.messages[key].pseudo} />
+                      </CSSTransition>))
     console.log(messages)
     return (
       <div className='box'>
         <div>
-          <div className='message'ref={this.messageRef}>
-            {messages}
-          </div>
+          <TransitionGroup className='message'ref={this.messageRef}>
+            
+              {messages}
+            </TransitionGroup>
         </div>
         <Formulaire
         length={150}
--- chatbox-app/src/components/Message.js
+++ chatbox-app/src/components/Message.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
-const Message = ({pseudo, message})=> { // recuperation des props
-    return (
-        <p className="user-message">
-            {message}
-        </p>
-    )
+const Message = ({pseudo, message, isUser})=> { // recuperation des props
+    
+// ici on fait une condition pour differencier les differents utulisateurs
+
+    if(isUser(pseudo)){
+        return (
+            <p className="user-message">
+                {message}
+            </p>
+        )
+    }
+    else {
+        return (
+            <p className="not-user-message">
+                <strong>{pseudo} :</strong>{message}
+            </p>
+        )
+    }
+    
 }
 
 export default Message
