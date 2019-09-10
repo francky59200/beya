56d834473e30bbeb7a7f3265c0af7de06744ab9b
francky59200 <befranck74@gmail.com>
2019-09-10 23:33:45 +0200

    modification du css (React)

--- chatbox-app/src/index.js
+++ chatbox-app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import Connexion from './components/Connexion'
 import NotFound from './components/NotFound'
