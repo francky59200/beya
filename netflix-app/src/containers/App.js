17689e27a736a26436095093dc93f44c09e85cb5
francky59200 <befranck74@gmail.com>
2019-09-13 01:23:12 +0200

    mise en place des composants et containers

---
+++ netflix-app/src/containers/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+
+const App =function () {
+  return <div className="App">salut les amis comment ça va</div>
+    
+}
+
+export default App;
---
+++ netflix-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './containers/App';
+
+const API_KEY="4ad9a0bdd1ad58494fcb90762f2b79ae"
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
