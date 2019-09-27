911cb2be00e4fbc529615b75ef0c24e103a09fa1
francky59200 <befranck74@gmail.com>
2019-09-28 01:08:16 +0200

    Rendu de manière récursive de données imbriquées (React)

--- data-json/src/App.js
+++ data-json/src/App.js
@@ -2,15 +2,92 @@ import React from 'react';
 import './App.css';
 import PostData from './postdata'
 
-const DATA = require('./data.json')
+const DATA = [
+  {
+      "id": "LIB1",
+      "name": "Library 1",
+      "context": "C1",
+      "children": [
+          {
+              "id": "SKI1",
+              "name": "SKill 1",
+              "context": "C1",
+              "children": [
+                  {
+                      "id": "SKI11",
+                      "name": "SKill 11",
+                      "context": "C1"
+                  },
+                  {
+                      "id": "SKI12",
+                      "name": "SKill 12",
+                      "context": "C1",
+                      "children": []
+                  },
+                  {
+                      "id": "SKI13",
+                      "name": "SKill 13",
+                      "context": "C1",
+                      "children": [
+                          {
+                              "id": "SKI131",
+                              "name": "SKill 131",
+                              "context": "C1",
+                              "children": [
+                                  {
+                                      "id": "SKI1311",
+                                      "name": "SKill 1311",
+                                      "context": "C1",
+                                      "children": [
+                                          {
+                                              "id": "SKI13111",
+                                              "name": "SKill 13111",
+                                              "context": "C1"
+                                          }
+                                      ]
+                                  }
+                              ]
+                          }
+                      ]
+                  }
+              ]
+          },
+          {
+              "id": "SKI2",
+              "name": "SKill 2",
+              "context": "C1",
+              "children": [
+                  {
+                      "id": "SKI21",
+                      "name": "SKill 21",
+                      "context": "C1",
+                      "children": [
+                          null
+                      ]
+                  },
+                  {
+                      "id": "SKI22",
+                      "name": "SKill 22",
+                      "context": "C1"
+                  }
+              ]
+          }
+      ]
+  }
+]
 
 
 function App() {
   return (
-    <div className="App">
-        <PostData data={DATA}/>
+    <div>
+      {
+        DATA.map((comment) => {
+          return (
+            <PostData key={comment.id} comment={comment} />
+          )
+        })
+      }
     </div>
-  );
+  )
 }
-
 export default App;
---
+++ data-json2/src/App.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const side = {
+  "children": [
+    {
+      "children": [
+        {
+          "id": "b123a",
+          "name": "compo"
+        },
+        {
+          "id": "5a523",
+          "name": "orient"
+        },
+        {
+          "id": "42e7e",
+          "name": "build"
+        }
+      ],
+      "id": "f9ca7",
+      "name": "building"
+    },
+    {
+      "children": [
+        {
+          "id": "dbd4",
+          "name": "insula"
+        },
+        {
+          "id": "ad8404",
+          "name": "fenest"
+        },
+        {
+          "id": "e3837",
+          "name": "window"
+        }
+      ],
+      "id": "af776",
+      "name": "envelope"
+    },
+    {
+      "children": [
+        {
+          "id": "ba515",
+          "name": "heating"
+        },
+        {
+          "id": "6618",
+          "name": "cooling"
+        },
+        {
+          "id": "4a0547b8f",
+          "name": "water_heating_system"
+        }
+      ],
+      "id": "3e967",
+      "name": "systems"
+    },
+    {
+      "id": "e7c6e",
+      "name": "spatial"
+    },
+    {
+      "id": "9f641",
+      "name": "design"
+    }
+  ],
+  "name": "root"
+}
+
+// Rendu recursif de donnée json 
+
+
+const SideMenu = ({data}) => {
+ 
+  return (
+    <ul>
+      {
+      Array.isArray(data)? data.map(item => { // on vérifie si c'est un tableau de donnée si c'est le cas on fait une boucle
+        return (<li> {/* on recupére les name children parent*/}
+          {item.name}
+          {item.children && <SideMenu data={item.children} />}
+        </li>);
+      }): <li>
+          {data.name} {/*on recupére les name parents*/}
+          {data.children && <SideMenu data={data.children} />}
+        </li>
+      }
+    </ul>
+  );
+}
+
+function App() {
+  return (
+   <SideMenu data={side}/>
+  )
+}
+ 
+export default App
