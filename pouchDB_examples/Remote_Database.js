9477fa64db572bcc535edb7cac13bb61fa132995
francky59200 <befranck74@gmail.com>
2019-10-25 15:22:57 +0200

    insert a lot of documents

--- pouchDB_examples/Remote_Database.js
+++ pouchDB_examples/Remote_Database.js
@@ -8,15 +8,29 @@ let db = new PouchDB('http://localhost:5984/employes')
 
 // on cree un document
 
-let doc={
-    name: "françois",
-    age: "35",
-    ville:"tourcoing"
-}
+let doc1={name: "françois", age: "35", ville:"tourcoing"}
+let doc2={name: "macarena", age: "32", ville:"lille"}
+let doc3={name: "vincent", age: "31", ville:"roubaix"}
+let doc4={name: "jules", age: "30", ville:"calais"}
+
+let docs=[doc1, doc2, doc3, doc4 ]
+
+
 
 // on insere dans la base de donnée
 
-db.post(doc, function(err, res){
+// insertion d'un lot de documents dans la base de donnée
+
+db.bulkDocs(docs, function(err, res){
+    if(err){
+        return console.log(err)
+    }else{
+        console.log("your documents are successfully insert")
+    }
+})
+
+// avec db.post() pouchdb génère les id a chaque element, ce qui est different de db.put()
+/*db.post(doc, function(err, res){
     if(err){
        return console.log(err)
     }else{
@@ -24,7 +38,7 @@ db.post(doc, function(err, res){
     }
 })
 
-/*db.info(function(err,info){
+db.info(function(err,info){
     if(err){
         return console.log(err)
     }else{
