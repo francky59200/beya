4c01a19b8cf5df9162b55e2dbb1b846c45c2cf3c
francky59200 <befranck74@gmail.com>
2019-10-26 02:56:30 +0200

    data display

---
+++ vuejs_project/app.js
@@ -0,0 +1,11 @@
+"use strict"
+
+new Vue({
+    el: '#app',
+    data:{
+        message: 'bonjour comment tu vas',
+        link: 'https://franckbeya.fr',
+        success: false,
+        persons:['pierre', 'eric', 'jonathan', 'lucas', 'marc']
+    }
+})
